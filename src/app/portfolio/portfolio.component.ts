134baee94bb1a859300595d4d92396840fc280c0
Vivek <vivekdoshi11@gmail.com>
2025-03-15 23:35:15 +0530

    Angular basic setup
    Basic setup ready

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [CommonModule],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.scss'
+})
+export class PortfolioComponent {
+  images: number[] = Array.from({ length: 30 }, (_, index) => index + 1);
+}
